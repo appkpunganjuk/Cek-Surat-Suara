0015059f5c00cf4b333d8596d7a1014f2e8e3c65
appkpunganjuk <app.kpunganjuk@gmail.com>
2023-08-04 15:35:51 +0700

    Update 20230804 15.36

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
-
-  $('#wa-kabko').select2()
-  $('#wa-kec').select2()
-  $('#wt-kec').select2()
-  $('#wa-prov').select2()
+  loadwaprov()
+  loadwakabko()
+  loadwakec()
+  loadwtkec()
 
   var wa_prov = ''
   var wa_kabko = ''
   var wa_kec = ''
+
+  // For sweetalert settings
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-primary',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+
+  // sweetalert calling
+  function peringatan(judul,pesan,icon) {
+    swalWithBootstrapButtons.fire({
+      title: judul,
+      text: pesan,
+      icon: icon,
+      showCancelButton: false,
+      confirmButtonText: '&nbsp; OK &nbsp;',
+      cancelButtonText: '',
+      reverseButtons: true
+    })
+  }
+
   // load data from JSON file
   var ss = [
     {'ssname':'Anggota DPR','state':'<i class="fas"></i>'}, 
@@ -16,78 +38,143 @@ $(document).ready(function() {
     {'ssname':'Anggota DPRD Provinsi','state':'<i class="fas fa-times"></i>'},
     {'ssname':'Anggota DPRD Kabupaten/Kota','state':'<i class="fas fa-times"></i>'}]
 
-  $.getJSON('db_dapil.json', function (data) {
-    console.log(data.PROVINSI)
-    // combobox wa-prov
-    for (var h=0; h<data.PROVINSI.length; h++) {
-      let opt = document.createElement('option')
-      opt.text = data.PROVINSI[h].PROVINSI
-      if (data.PROVINSI[h].PROVINSI == "Jawa Timur"){
-        opt.selected = true
+  
+  // combobox wa-prov
+  function loadwaprov(e) {
+    $('#wa-prov').find('option').remove()
+    $.getJSON('db_dapil.json', function(data) {
+      for (var h=0; h<data.PROVINSI.length; h++) {
+        let opt = document.createElement('option')
+        opt.text = data.PROVINSI[h].PROVINSI
+        opt.value = data.PROVINSI[h].ID
+        if(e) {
+          if (data.PROVINSI[h].PROVINSI == e){
+            opt.selected = true
+          }
+        } else {
+          if (data.PROVINSI[h].PROVINSI == 'Jawa Timur'){
+            opt.selected = true
+          }
+        }
+        $('#wa-prov').append(opt)
       }
-      $('#wa-prov').append(opt)
-    }
-    wa_prov = $('#wa-prov').val()
+    })
+    $('#wa-prov').select2()
+    wa_prov = '35'
+  }
 
     // combobox wa-kabko
-    for (var i = 0; i < data.DAPIL_JATIM.length; i++) {
-      let opt = document.createElement('option')
-      opt.text = data.DAPIL_JATIM[i].KABKO
-      opt.value = data.DAPIL_JATIM[i].ID
-      $('#wa-kabko').append(opt)
-    }
-    wa_kabko = $('#wa-kabko').val()
+  function loadwakabko(e) {
+    $('#wa-kabko').find('option').remove()
+    $.getJSON('db_dapil.json', function(data) {
+      for (var i = 0; i < data.DAPIL_JATIM.length; i++) {
+        let opt = document.createElement('option')
+        opt.text = data.DAPIL_JATIM[i].KABKO
+        opt.value = data.DAPIL_JATIM[i].ID
+        if (e) {
+          if (data.DAPIL_JATIM[i].KABKO == e){
+            opt.selected = true
+          }
+        } else {
+          if(data.DAPIL_JATIM[i].KABKO == 'Nganjuk') {
+            opt.selected = true
+          }
+        }
+        $('#wa-kabko').append(opt)
+      }
+    })
+    $('#wa-kabko').select2()
+    wa_kabko = '18'
+  }
 
     // combobox wa-kec dan wt-kec
-    for (var j = 0; j < data.DAPIL_NGANJUK.length; j++) {
-      let opt = document.createElement('option')
-      opt.text = data.DAPIL_NGANJUK[j].KECAMATAN
-      opt.value = data.DAPIL_NGANJUK[j].DPRDKABKO
-      let opt2 = document.createElement('option')
-      opt2.text = opt.text
-      opt2.value = opt.value
-      $('#wt-kec').append(opt)
-      $('#wa-kec').append(opt2)
-    }
-    wa_kec = $('#wa-kec').val()
-  });
+  function loadwakec(e) {
+    $('#wa-kec').find('option').remove()
+    $.getJSON('db_dapil.json', function(data) {
+      for (var j = 0; j < data.DAPIL_NGANJUK.length; j++) {
+        let opt = document.createElement('option')
+        opt.text = data.DAPIL_NGANJUK[j].KECAMATAN
+        opt.value = data.DAPIL_NGANJUK[j].DPRDKABKO
+        if(e) {
+          if(data.DAPIL_NGANJUK[j].KECAMATAN == e) {
+            opt.selected = true
+          }
+        }
+        $('#wa-kec').append(opt)
+      }
+    })
+    $('#wa-kec').select2()
+  }
+
+  function loadwtkec() {
+    $('#wt-kec').find('option').remove()
+    $.getJSON('db_dapil.json', function(data) {
+      for (var j = 0; j < data.DAPIL_NGANJUK.length; j++) {
+        let opt = document.createElement('option')
+        opt.text = data.DAPIL_NGANJUK[j].KECAMATAN
+        opt.value = data.DAPIL_NGANJUK[j].DPRDKABKO
+        $('#wt-kec').append(opt)
+      }
+    })
+    $('#wt-kec').select2()
+  }
 
   $('#wa-prov').change(function() {
     wa_prov = $(this).val()
-    if ($(this).val() == 'Jawa Timur' || $(this).val() == 'Luar Negeri') {
+    if ($(this).val() == '35' || $(this).val() == '00') {
+      loadwakabko('Nganjuk')
+      loadwakec()
       $('#wa-kabko').prop('disabled', false)
       $('#wa-kec').prop('disabled', false)
-      wa_kabko = $('#wa-kabko').val()
-      wa_kec = $('#wa-kec').val()
     } else {
+      loadwakabko('-')
+      loadwakec('-')
       $('#wa-kabko').prop('disabled', true)
       $('#wa-kec').prop('disabled', true)
-      wa_kabko = ''
-      wa_kec = ''
     }
+
+    if($(this).val() == '00') {
+      peringatan('Informasi','Untuk pemilih pindahan dari Luar Negeri/ TPS Lokasi Khusus silahkan sesuaikan wilayah asal dengan dokumen KTP/ Kartu Keluarganya.','info')
+    }
+    wa_prov = $('#waprov').val()
+    wa_kabko = $('#wa-kabko').val()
+    wa_kec = $('#wa-kec').val()
   })
 
   $('#wa-kabko').change(function() {
     wa_kabko = $(this).val()
     if ($(this).val() !== '18') {
+      loadwakec("-")
       $('#wa-kec').prop('disabled', true)
-      wa_kec = ''
     } else {
+      loadwakec('')
       $('#wa-kec').prop('disabled', false)
-      wa_kec = $('#wa-kec').val()
     }
+    wa_prov = $('#waprov').val()
+    wa_kabko = $('#wa-kabko').val()
+    wa_kec = $('#wa-kec').val()
   })
 
   $('#wa-kec').change(function() {
-    wa_kec = $(this).val()
+    wa_prov = $('#waprov').val()
+    wa_kabko = $('#wa-kabko').val()
+    wa_kec = $('#wa-kec').val()
+  })
+
+  $('#wt-kec').change(function() {
+    wa_prov = $('#waprov').val()
+    wa_kabko = $('#wa-kabko').val()
+    wa_kec = $('#wa-kec').val()
   })
 
   $('#btn-cek').click(function(){
     ss_default()
-    if ($('#wt-kec').val()=='') {
-      alert('Pilih Wilayah Tujuan dahulu')
-    } else if ($('#wt-kec').val() !=='' && wa_kabko == '18' && wa_kec =='') {
-      alert('Pilih kecamatan di wilayah asal dahulu')
+    if ($('#wt-kec').val()=='0') {
+      // alert('Pilih Wilayah Tujuan dahulu')
+      peringatan('Informasi','Pilih Wilayah Tujuan dahulu','info')
+    } else if ($('#wt-kec').val() !=='0' && wa_kabko == '18' && wa_kec =='0') {
+      // alert('Pilih kecamatan di wilayah asal dahulu')
+      peringatan('Informasi','Pilih kecamatan di wilayah asal dahulu','info')
     } else {
       $.getJSON('db_dapil.json', function (data) {
         // compare dapil jatim
@@ -107,7 +194,7 @@ $(document).ready(function() {
           ss[4].state = '<i class="fas fa-check"></i>'
         }
         // create the output
-        var opstr = '<div id="op" class="table-responsive"><table class="table table-bordered"><tbody>'
+        var opstr = '<div id="op" class="table-responsive">Surat Suara yang didapat:<br><table class="table table-bordered"><tbody>'
         for(var x = 0; x < ss.length; x++) {
           if(ss[x].state == '<i class="fas fa-times"></i>') {
             opstr += '<tr><td style="text-decoration:line-through">' + ss[x].ssname + '</td><td class="text-center bg-warning text-light">'+ ss[x].state + '</td></tr>'
@@ -120,7 +207,6 @@ $(document).ready(function() {
         if($('#op')) {
           $('#op').remove()
         }
-        $('#res-box').append('Surat Suara yang didapat:<br>')
         $('#res-box').append(opstr)
         location.href = '#end-page'
       })
@@ -133,4 +219,7 @@ $(document).ready(function() {
     ss[4].state='<i class="fas fa-times"></i>'
   }
 
+  $('#bantuan').click(function(){
+    peringatan('Bantuan','Isikan wilayah tujuan sesuai dengan lokasi anda sekarang. Isikan wilayah asal sesuai dengan wilayah asal pemilih yang melakukan pindah pilih. Khusus pemilih pindahan dari luar negeri atau TPS lokasi khusus silahakan pilih "Luar Negeri / TPS Lokasi Khusus" pada isian provinsi dan sesuakan kabupaten/kota hingga kecamatan dengan dokumen KTP nya','question')
+  })
 });
